fbb7434974744c1123452a659b20c7419c2532cb
kassimma123 <katpiskorz@student.agh.edu.pl>
2026-01-24 14:24:16 +0100

    dodanie podstrony About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,50 @@
-function About() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './css/About.module.css';
 
-}
+const About = () => {
+    return (
+        <div className={styles.aboutWrapper}>
+            <div className={styles.aboutCard}>
+                {/* Glitch Title */}
+                <h1 className={styles.glitchTitle} data-text="MISSION_STATEMENT">
+                    MISSION_STATEMENT
+                </h1>
+
+                <div className={styles.content}>
+                    <p className={styles.manifesto}>
+                        Welcome to <span className={styles.highlight}>VHS_CLUB</span>.
+                        We are more than just a store; we are a sanctuary for celluloid enthusiasts.
+                        Our mission is to uphold <span className={styles.bold}>traditional film values</span> in an
+                        increasingly digital world.
+                    </p>
+
+                    <p className={styles.description}>
+                        We believe in the texture of magnetic tape and the soul of analog playback.
+                        While we celebrate the classics, we constantly supplement our unique collection
+                        with <span className={styles.bold}>modern productions</span>, proving that the VHS format
+                        is timeless.
+                    </p>
+
+                    <div className={styles.ctaSection}>
+                        <p className={styles.suggestion}>SELECT YOUR NEXT MOVE:</p>
+
+                        {/* Nawigacja wewnątrz strony */}
+                        <div className={styles.internalNav}>
+                            <Link to="/shop" className={styles.navBtn}>[VIEW_SHOP]</Link>
+                            <Link to="/contact" className={styles.navBtn}>[CONTACT_US]</Link>
+                            <Link to="/reviews" className={styles.navBtn}>[REVIEWS]</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className={styles.footerInfo}>
+                    * ESTABLISHED 1998 // UPDATED 2026
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default About;
